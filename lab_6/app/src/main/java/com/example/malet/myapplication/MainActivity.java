522b7827eec54f047d57f507e46d3503d46b27a1
Pavel <maletich.p.i.1.16@gmail.com>
2018-05-08 08:36:51 +0300

    ffs

---
+++ lab_6/app/src/main/java/com/example/malet/myapplication/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.malet.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tvInfo, nLevel;
+    EditText etInput;
+    Button bControl;
+    int rand;
+    boolean finish = false;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvInfo = (TextView)findViewById(R.id.textView);
+        etInput = (EditText)findViewById(R.id.editText);
+        bControl = (Button)findViewById(R.id.button);
+        rand =  (int)(Math.random()*100);
+
+    }
+    public void onClick(View v) {
+        if(etInput.getText().length() != 0) {//для пустого поля
+            if (!finish) {
+                int input = Integer.parseInt(etInput.getText().toString());
+                if(input > 0 && input <= 100) {
+                    if (input > rand) {
+                        tvInfo.setText(getResources().getString(R.string.ahead));
+                    }
+                    if (input < rand) {
+                        tvInfo.setText(getResources().getString(R.string.behind));
+                    }
+                    if (input == rand) {
+                        tvInfo.setText(getResources().getString(R.string.hit));
+                        bControl.setText(getResources().getString(R.string.play_more));
+                        finish = true;
+                    }
+                }
+                else
+                {
+                    if(input <= 0)
+                    {
+                        Toast.makeText(
+                                MainActivity.this,"Вводимое число меньше нуля!",
+                                Toast.LENGTH_SHORT
+                        ).show();
+                    }
+                    if(input > 100)
+                    {
+                        Toast.makeText(
+                                MainActivity.this,"Вводимое число больша 100!",
+                                Toast.LENGTH_SHORT
+                        ).show();
+                    }
+                }
+            } else {
+                rand = (int) (Math.random() * 100);
+                tvInfo.setText(getResources().getString(R.string.try_to_guess));
+                bControl.setText(getResources().getString(R.string.input_value));
+                finish = false;
+            }
+            etInput.setText("");
+        }else
+        {
+            Toast.makeText(
+                    MainActivity.this,"Поле пустое!",
+                    Toast.LENGTH_SHORT
+            ).show();
+        }
+    }
+}
